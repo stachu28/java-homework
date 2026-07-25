c4cecec0b4b44d4fdee87bed38091132abe4abbc
Jakub Stachon <stachu28@gmail.com>
2026-07-25 13:48:16 +0200

    7. **Instance method**     - Add a method `introduceYourself()` that prints:       `I am a car of brand BRAND from year YEAR.`     - Call it in `main` for several objects.

--- Java Homework/src/Task5/Main.java
+++ Java Homework/src/Task5/Main.java
@@ -1,28 +1,28 @@
 package Task5;
 
 public class Main {
-    static void main(String[] args) {
-//        System.out.println("*** 1 ***");
-//        Car car1 = new Car("Audi", 2024);
-//        System.out.println(car1.toString());
-//
-//        System.out.println("\n *** 2 ***");
-//        Car car2 = new Car();
-//        System.out.println(car2);
-//
-//        System.out.println("\n *** 3 ***");
-//        Car car3 = new Car("BMW", 2023);
-//        Car car4 = new Car("VW", 2021);
-//        Car car5 = new Car("Porsche", 2019);
-//
-//        System.out.println("\n *** 4 ***");
-//        car3.showDifference();
-//        car4.showDifference();
-//
-//        System.out.println("\n *** 5 ***");
-//        System.out.println(car5.brand);
-//        car5.changeBrand("Audi");
-//        System.out.println(car5.brand);
+    public static void main(String[] args) {
+        System.out.println("*** 1 ***");
+        Car car1 = new Car("Audi", 2024);
+        System.out.println(car1.toString());
+
+        System.out.println("\n *** 2 ***");
+        Car car2 = new Car();
+        System.out.println(car2);
+
+        System.out.println("\n *** 3 ***");
+        Car car3 = new Car("BMW", 2023);
+        Car car4 = new Car("VW", 2021);
+        Car car5 = new Car("Porsche", 2019);
+
+        System.out.println("\n *** 4 ***");
+        car3.showDifference();
+        car4.showDifference();
+
+        System.out.println("\n *** 5 ***");
+        System.out.println(car5.brand);
+        car5.changeBrand("Audi");
+        System.out.println(car5.brand);
 
         System.out.println("\n *** 6 ***");
         Car car6 = new Car();
