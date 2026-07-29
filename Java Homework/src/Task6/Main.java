11541852e7268a818aa18f9a978f8638e3bf1130
Jakub Stachon <stachu28@gmail.com>
2026-07-29 12:53:11 +0200

    5. **Enum z metodą**     - Dodaj w `DayOfWeek` metodę `isWeekend()`, która zwraca `true` dla SATURDAY i SUNDAY.     - W `main` sprawdź działanie dla kilku dni.

--- Java Homework/src/Task6/Main.java
+++ Java Homework/src/Task6/Main.java
@@ -16,17 +16,27 @@ public class Main {
         DayOfWeek day2 = DayOfWeek.MONDAY;
         DayOfWeek day3 = DayOfWeek.TUESDAY;
 
-        System.out.println("Is 'day1' same as 'day2': ");
+        System.out.println("Is 'day1' same as 'day2'?");
         System.out.println(day1 == day2);
-        System.out.println("Is 'day1' same as 'day3': ");
+        System.out.println("Is 'day1' same as 'day3'?");
         System.out.println(day1 == day3);
 
         System.out.println("\n*** 4 ***");
-        DayOfWeek day = DayOfWeek.WEDNESDAY;
-        switch (day) {
+        DayOfWeek day4 = DayOfWeek.WEDNESDAY;
+        switch (day4) {
             case MONDAY -> System.out.println("The beginning of the week!");
             case TUESDAY, WEDNESDAY, THURSDAY, FRIDAY -> System.out.println("Mid-week!");
             case SATURDAY, SUNDAY -> System.out.println("Weekend!");
         }
+
+        System.out.println("\n*** 5 ***");
+        DayOfWeek day5 = DayOfWeek.MONDAY;
+        DayOfWeek day6 = DayOfWeek.SATURDAY;
+        System.out.println("Is 'day5' a weekend?");
+        System.out.println(DayOfWeek.isWeekend(day5));
+        System.out.println("Is 'day5' a weekend?");
+        System.out.println(DayOfWeek.isWeekend(day6));
+
+
     }
 }
