8156b6a181fd10da5f4a3a0b4b309d19c3f0d35a
Jakub Stachon <stachu28@gmail.com>
2026-07-29 12:20:40 +0200

    1. **Prosty enum – dni tygodnia**     - Utwórz `enum DayOfWeek` z wartościami: MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY.     - W `main` przypisz zmiennej dowolny dzień i wypisz go.

---
+++ Java Homework/src/Task6/DayOfWeek.java
@@ -0,0 +1,5 @@
+package Task6;
+
+public enum DayOfWeek {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
---
+++ Java Homework/src/Task6/Main.java
@@ -0,0 +1,10 @@
+package Task6;
+
+public class Main {
+    static void main(String[] args) {
+        System.out.println("*** 1 ***");
+        DayOfWeek today = DayOfWeek.WEDNESDAY;
+
+        System.out.println("Today is: " + today);
+    }
+}
