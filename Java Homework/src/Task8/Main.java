a23cf7ffae205603919ee0929e4188dce170e0b2
Jakub Stachon <stachu28@gmail.com>
2026-08-03 23:19:50 +0200

    3. **Międzystrefowy (WAW → NYC)**
        * Wylot: `2025-01-15 10:00:00 (Europe/Warsaw)`
        * Przylot: `2025-01-15 14:00:00 (America/New_York)`
        * **Oczekiwane:** `10h`
          *Wyjaśnienie: 09:00Z → 19:00Z.*

--- Java Homework/src/Task8/Main.java
+++ Java Homework/src/Task8/Main.java
@@ -5,19 +5,32 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 public class Main {
+    private static Duration duration;
+
     static void main(String[] args) {
         Task1();
         Task2();
+        System.out.println("\n*** 3 ***");
+        ZonedDateTime departure = ZonedDateTime.of(2025, 01, 15, 10, 00, 00, 00, ZoneId.of("Europe/Warsaw"));
+        ZonedDateTime arrival = ZonedDateTime.of(2025, 01, 15, 14, 00, 00, 00, ZoneId.of("America/New_York"));
 
+        Flight flight = new Flight("LO103", "WAW", "WAW", departure, arrival);
+        Duration duration = flight.calculateFlightDuration();
+        System.out.println(
+                duration.toHours() + "h " +
+                        (duration.toMinutes() % 60) + "m");
     }
 
     private static void Task2() {
+        System.out.println("\n*** 1 ***");
         ZonedDateTime departure = ZonedDateTime.of(2025, 01, 15, 23, 00, 00, 00, ZoneId.of("Europe/Warsaw"));
         ZonedDateTime arrival = ZonedDateTime.of(2025, 01, 16, 01, 30, 00, 00, ZoneId.of("Europe/Warsaw"));
 
         Flight flight = new Flight("LO102", "WAW", "WAW", departure, arrival);
-
-
+        Duration duration = flight.calculateFlightDuration();
+        System.out.println(
+                duration.toHours() + "h " +
+                        (duration.toMinutes() % 60) + "m");
     }
 
     private static void Task1() {
@@ -27,16 +40,10 @@ public class Main {
 
         Flight flight = new Flight("LO101", "WAW", "WAW", departure, arrival);
 
-        System.out.println(flight.calculateFlightDuration());
-
         Duration duration = flight.calculateFlightDuration();
 
         System.out.println(
                 duration.toHours() + "h " +
-                        (duration.toMinutes() % 60) + "m"
-        );
+                        (duration.toMinutes() % 60) + "m");
     }
 }
-//    * Wylot: `2025-01-15 23:00:00 (Europe/Warsaw)`
-//        * Przylot: `2025-01-16 01:30:00 (Europe/Warsaw)`
-//        * **Oczekiwane:** `2h 30m`
