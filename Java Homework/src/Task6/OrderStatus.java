6db8371b264e75b3753df171365c289305389b69
Jakub Stachon <stachu28@gmail.com>
2026-08-03 21:33:55 +0200

    Task 7

--- Java Homework/src/Task6/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package Task6;
-
-public enum OrderStatus { NEW, PROCESSING, SHIPPED, DELIVERED, CANCELED;
-
-    public boolean isActive() {
-        return this != CANCELED && this != DELIVERED;
-    }
-}
