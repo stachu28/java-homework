0046a8a7b7adecf6b6c53d39bd6c559cedabdf4d
Jakub Stachon <stachu28@gmail.com>
2026-08-04 08:59:23 +0200

    7. **Nieprawidłowa strefa czasowa**
        * Próba: `ZoneId.of("Invalid/Zone")`
        * **Oczekiwane:** wyjątek `DateTimeException` (np. `ZoneRulesException`).

--- Java Homework/src/Task8/Main.java
+++ Java Homework/src/Task8/Main.java
@@ -1,5 +1,6 @@
 package Task8;
 
+import java.time.DateTimeException;
 import java.time.Duration;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -12,6 +13,50 @@ public class Main {
         Task2();
         Task3();
         Task4();
+        Task5();
+        Task6();
+        Task7();
+    }
+
+    private static void Task7() {
+        System.out.println("\n*** 7 ***");
+        ZonedDateTime departure;
+        ZonedDateTime arrival;
+
+        try {
+            departure = ZonedDateTime.of(
+                    2025, 1, 15, 10, 0, 0, 0,
+                    ZoneId.of("Invalid/Zone"));
+
+            arrival = ZonedDateTime.of(
+                    2025, 10, 26, 2, 30, 0, 0,
+                    ZoneId.of("Europe/Warsaw"));
+
+        } catch (DateTimeException e) {
+            System.out.println("Invalid time zone!");
+            return;
+        }
+
+        Flight flight = new Flight("LO107", "WAW", "WAW", departure, arrival);
+        Duration duration = flight.calculateFlightDuration();
+        System.out.println(
+                duration.toHours() + "h " +
+                        (duration.toMinutes() % 60) + "m");
+    }
+
+    private static void Task6() {
+        System.out.println("\n*** 6 ***");
+        ZonedDateTime departure = ZonedDateTime.of(2025, 01, 15, 10, 00, 00, 00, ZoneId.of("Europe/Warsaw"));
+        ZonedDateTime arrival = ZonedDateTime.of(2025, 10, 26, 02, 30, 00, 00, ZoneId.of("Europe/Warsaw"));
+
+        Flight flight = new Flight("LO106", "WAW", "WAW", departure, arrival);
+        Duration duration = flight.calculateFlightDuration();
+        System.out.println(
+                duration.toHours() + "h " +
+                        (duration.toMinutes() % 60) + "m");
+    }
+
+    private static void Task5() {
         System.out.println("\n*** 5 ***");
         ZonedDateTime departure = ZonedDateTime.of(2025, 10, 26, 01, 00, 00, 00, ZoneId.of("Europe/Warsaw"));
         ZonedDateTime arrival = ZonedDateTime.of(2025, 10, 26, 02, 30, 00, 00, ZoneId.of("Europe/Warsaw"));
